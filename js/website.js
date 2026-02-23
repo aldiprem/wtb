c741a0d138317e02e2f0a559d6b309385611f621
aldiprem <premiumaldi053@gmail.com>
2026-02-23 10:42:49 +0700

    Update website.js

--- js/website.js
+++ js/website.js
@@ -3,7 +3,7 @@
     console.log('🏪 Website Store - Initializing...');
 
     // ==================== KONFIGURASI ====================
-    const API_BASE_URL = 'https://dispatched-essex-treasures-reflects.trycloudflare.com';
+    const API_BASE_URL = 'https://intimate-benefit-editions-girls.trycloudflare.com';
     const DEFAULT_ENDPOINT = 'default-store';
 
     // ==================== DOM ELEMENTS ====================
