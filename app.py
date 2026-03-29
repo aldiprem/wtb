1ca55b679d3262d530adf686a9423f61ab555595
aldiprem <premiumaldi053@gmail.com>
2026-03-30 03:54:55 +0700

    g

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ def debug_info():
 if __name__ == '__main__':
     # Konfigurasi Domain
     DOMAIN = "companel.shop"
-    PORT = 80
+    PORT = 5050
     
     print("="*60)
     print("🚀 Starting Website Management API Server...")
