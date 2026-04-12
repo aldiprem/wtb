73c4067e2a9aea137a53ea22fbac14680c4c9c9b
aldiprem <premiumaldi053@gmail.com>
2026-04-13 02:45:05 +0700

    s

--- games/js/plinko_games.js
+++ games/js/plinko_games.js
@@ -23,7 +23,7 @@
     let balls = [];
     const GRAVITY = 0.2;
     const BOUNCE = 0.15;
-    const FRICTION_X = 0.35;
+    const FRICTION_X = 0.5;
     const PIN_RADIUS = 3;
     const BALL_RADIUS = 5;
     
