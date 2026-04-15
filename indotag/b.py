9c7059050804ae47a2939da548cfb42bb5bda159
aldiprem <premiumaldi053@gmail.com>
2026-04-16 02:26:11 +0700

    s

--- indotag/b.py
+++ indotag/b.py
@@ -9,7 +9,7 @@ from telethon import TelegramClient, events, Button
 env_path = Path(__file__).parent / '.env'
 load_dotenv(dotenv_path=env_path)
 
-from database import IndotagDatabase
+from database import data 
 
 # Logging
 logging.basicConfig(
